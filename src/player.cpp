4407c1679967c469d8023895811324e1ffc374b4
Enrique <869402@unizar.es>
2026-03-04 10:45:20 +0100

    Clase padre Entity

--- src/player.cpp
+++ src/player.cpp
@@ -1,17 +1,25 @@
 
 #include "player.hpp"
 
-Player::Player(glm::vec2 pos, glm::vec2 size, GLfloat velocity) {
-    this->position = pos;
-    this->speed = velocity;
-    this->size = size;
+Player::Player(glm::vec2 pos, glm::vec2 size, GLfloat velocity)
+    : Entity(pos, size, velocity) {}
+
+Player::~Player() {}
+
+void Player::Update() {
+    // Lógica de actualización del jugador (a completar)
+}
+
+void Player::Draw() {
+    // Lógica de renderizado del jugador (a completar)
 }
 
 void Player::UpdateSprite(Move mov) {
     switch (mov) {
-        case MOVE_UP: this->position += glm::vec2(0, -0.010f); break;
-        case MOVE_DOWN: this->position += glm::vec2(0, 0.010f); break;
-        case MOVE_LEFT: this->position += glm::vec2(-0.010f, 0); break;
-        case MOVE_RIGHT: this->position += glm::vec2(0.010f, 0); break;
+        case MOVE_UP:    this->position += glm::vec2( 0.0f,    -0.010f); break;
+        case MOVE_DOWN:  this->position += glm::vec2( 0.0f,     0.010f); break;
+        case MOVE_LEFT:  this->position += glm::vec2(-0.010f,   0.0f  ); break;
+        case MOVE_RIGHT: this->position += glm::vec2( 0.010f,   0.0f  ); break;
+        default: break;
     }
 }
